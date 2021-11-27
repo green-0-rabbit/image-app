9f95683c8319c2d44356f24dbb4d93b94b032c09
pyvvo <kenneth.ag@humaapi.com>
2021-11-27 20:00:32 +0100

    test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   images: {
-    domains: ['assets.vercel.com'],
-    formats: ['image/avif', 'image/webp'],
+    // domains: ['assets.vercel.com'],
+    // formats: ['image/avif', 'image/webp'],
+    reactStrictMode: true,
+    images: {
+      deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    },
   },
 }
